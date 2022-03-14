6fd17c0da447d67a473196bb121161415a1d79de
beginor <beginor@qq.com>
2022-03-14 17:43:51 +0800

    refact build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker buildx build --platform linux/amd64,linux/arm64 \
+docker buildx build \
+  --pull \
+  --platform linux/amd64,linux/arm64 \
   -t beginor/aria2:latest \
   -t beginor/aria2:1.36.0 \
   --push .
